b5eabe21bc5f658921d505405d9b8ff5b8689ebb
Kenz8489 <stark858981@gmail.com>
2025-11-05 01:21:51 +0530

    prj updated

--- app/components/slides.js
+++ app/components/slides.js
@@ -7,17 +7,17 @@ const slides = [
     url: "https://aot-blue.vercel.app/"
   },
   {
+    title: "localinsights",
+    description: "Users can search for any city, neighborhood, or country and instantly get a richly structured place profile generated and validated from multiple sources. The app surfaces featured destinations, supports infinite exploration, and produces human-readable place summaries using AI while keeping visual polish with theming and smooth animations.",
+    image: "/images/img-prj (3).webp",
+    url: "https://localinsights.vercel.app"
+  },
+  {
     title: "Avishkar",
-    description: "MERN stack project 2. An internal stack management dashboard designed to monitor app deployments, dependencies, and team workflows in real time.",
+    description: "Avishkar is the official website for our college’s annual technical fest, built to capture the energy, creativity, and innovation of the event in a visually engaging digital experience. Designed and developed using React.js, it combines smooth modern animations, responsive design, and an intuitive layout to deliver a seamless experience across all devices.",
     image: "/images/img-prj (2).webp",
     url: "https://www.avishkar.live/"
   },
-  {
-    title: "College Mock Test",
-    description: "MERN stack project 3. A next-gen documentation tool blending markdown, AI suggestions, and live previews — turning technical docs into living knowledge bases.",
-    image: "/images/img-prj (3).webp",
-    url: "https://musaliarcollege.in/"
-  }
 ];
 
 export default slides;
