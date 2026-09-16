d5a096fd847dc710150cc896d2a45c5e707db821
Ansif <stark858981@gmail.com>
2026-09-16 23:39:25 +0530

    commented unwanted import

--- app/sections/Skills.tsx
+++ app/sections/Skills.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
-import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision';
+// import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision';
 import { Bot, Code, GitBranch, Monitor, Server } from 'lucide-react';
 
 const Skills = () => {
